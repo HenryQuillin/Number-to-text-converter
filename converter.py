73d0b9539cd428bc8c6c05dd84db59b50c058cf6
HenryQuillin <henryquillin@gmail.com>
2020-09-03 20:24:35 -0500

    Added scripts

--- converter.py
+++ converter.py
@@ -1,2 +1,17 @@
-# Num to test converter
-print('this is from my mac')
+# Num to text converter
+number = (input('Enter numbers: '))
+dic = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine"
+}
+output = ""
+for e in number:
+    output += dic.get(e) + " "
+print(output)
